1ba3196d8240bb57d2e39cf339fb2a4760bdc42b
Zhang Peng <forbreak@163.com>
2018-11-06 13:13:10 +0800

    更新代码

---
+++ common/src/main/java/io/github/dunwu/utils/base/annotation/NotNull.java
@@ -0,0 +1,8 @@
+package io.github.dunwu.utils.base.annotation;
+
+/**
+ * 标注参数不可为Null
+ */
+public @interface NotNull {
+
+}
---
+++ common/src/main/java/io/github/dunwu/utils/base/annotation/Nullable.java
@@ -0,0 +1,8 @@
+package io.github.dunwu.utils.base.annotation;
+
+/**
+ * 标注参数可为Null
+ */
+public @interface Nullable {
+
+}
