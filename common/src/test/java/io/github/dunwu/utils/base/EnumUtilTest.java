ed40b4cbbd5bcc481126d7950ca8ca42992bd46e
Zhang Peng <forbreak@163.com>
2018-11-08 10:46:37 +0800

    单元测试

---
+++ common/src/test/java/io/github/dunwu/utils/base/EnumUtilTest.java
@@ -0,0 +1,39 @@
+package io.github.dunwu.utils.base;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+import io.github.dunwu.utils.collection.ListUtil;
+
+public class EnumUtilTest {
+
+    public enum Options {
+        A,
+        B,
+        C,
+        D;
+    }
+
+    @Test
+    public void testBits() {
+        assertThat(EnumUtil.generateBits(Options.class, Options.A)).isEqualTo(1);
+        assertThat(EnumUtil.generateBits(Options.class, Options.A, Options.B)).isEqualTo(3);
+
+        assertThat(EnumUtil.generateBits(Options.class, ListUtil.newArrayList(Options.A))).isEqualTo(1);
+        assertThat(EnumUtil.generateBits(Options.class, ListUtil.newArrayList(Options.A, Options.B))).isEqualTo(3);
+
+        assertThat(EnumUtil.processBits(Options.class, 3)).hasSize(2).containsExactly(Options.A, Options.B);
+        assertThat(
+            EnumUtil.processBits(Options.class, EnumUtil.generateBits(Options.class, Options.A, Options.C, Options.D)))
+            .hasSize(3).containsExactly(Options.A, Options.C, Options.D);
+
+    }
+
+    @Test
+    public void testString() {
+        assertThat(EnumUtil.toString(Options.A)).isEqualTo("A");
+
+        assertThat(EnumUtil.fromString(Options.class, "B")).isEqualTo(Options.B);
+    }
+
+}
