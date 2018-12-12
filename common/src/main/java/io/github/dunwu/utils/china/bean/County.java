443facc565202140507c80651d69844d11b58343
Zhang Peng <forbreak@163.com>
2018-12-12 11:15:52 +0800

    省市区工具类

---
+++ common/src/main/java/io/github/dunwu/utils/china/bean/County.java
@@ -0,0 +1,67 @@
+package io.github.dunwu.utils.china.bean;
+
+/**
+ * @author Zhang Peng
+ * @date 2018-12-11
+ */
+public class County implements Comparable<County> {
+    private String code;
+    private String name;
+    private City city;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    @Override
+    public int compareTo(County o) {
+        return this.getCode().compareToIgnoreCase(o.getCode());
+    }
+
+    @Override
+    public int hashCode() {
+        int prime = 31;
+        int result = 1;
+        result = prime * result + ((code == null) ? 0 : code.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        County county = (County) obj;
+        if (county.getCode().equals(this.getCode())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
