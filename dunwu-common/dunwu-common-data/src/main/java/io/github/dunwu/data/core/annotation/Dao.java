b6c16334c403f8c8c6d21a59bde5873d7c5e40f7
Zhang Peng <forbreak@163.com>
2020-05-17 13:43:24 +0800

    temp

--- dunwu-common/dunwu-common-data/src/main/java/io/github/dunwu/data/core/annotation/Dao.java
+++ dunwu-common/dunwu-common-data/src/main/java/io/github/dunwu/data/core/annotation/Dao.java
@@ -3,23 +3,18 @@ package io.github.dunwu.data.core.annotation;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Component;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * Dao 标记注解
- * <p>
  *
  * @author <a href="mailto:forbreak@163.com">Zhang Peng</a>
  * @since 2019-07-23
  */
-@Target({ ElementType.TYPE })
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
 @Component
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
 public @interface Dao {
 
     @AliasFor(annotation = Component.class)
